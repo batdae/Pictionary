0b9e7da0aa11b2dac32cdc027e0ebbffb48cb12c
KiraTheDev <basiledd@gmail.com>
2020-04-30 15:50:35 +0200

    final

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 'use strict';
-//js plus restrictif
 
 const express = require('express');
 const socketIO = require('socket.io');
@@ -8,46 +7,71 @@ const port = process.env.PORT || 3000;
 const index = '/pictionary.html';
 
 const server = express()
-  .use((req, res) => res.sendFile(index, { root: __dirname }))
-  .listen(port, () => console.log('Server started on port ', port));
+    .use((req, res) => res.sendFile(index, { root: __dirname }))
+    .listen(port, () => console.log('Server started on port', port));
 
 const io = socketIO(server);
 
 let users = [];
+let currentPlayer = null;
+let timeout = null;
+const words = ['Pomme', 'Arbre', 'Voiture', 'Palmier', 'Soleil', 'Crayon', 'Bic','Cerise'];
 
 io.on('connection', (socket) => {
-  console.log('a new user joined the game');
+    console.log('A new user joined the game');
 
-  onConnection(socket);
-})
-
+    onConnection(socket);
+});
 
 function onConnection(socket) {
+    socket.on('username', (username) => {
+        console.log('Player name :', username);
+        socket.username = username;
 
-  socket.on('username', (username) => {
-    console.log("player name : ", username)
-    socket.username = username;
-    users.push(socket);
-    sendUsers();
-  })
-  
-  socket.on('disconnect', () => {
-    console.log("a user left the game")
-    users = users.filter(user => {
-      return user !== socket;
+        if (users.length === 0) {
+            currentPlayer = socket;
+            users.push(socket);
+            switchPlayer();
+        } else {
+            users.push(socket);
+            sendUsers();
+        }
     });
-    sendUsers();
-  })
+    socket.on('disconnect', (username) => {
+        console.log('A user left the game');
+        users = users.filter(user => {
+            return user !== socket;
+        });
+        sendUsers();
 
-  socket.on('line', data => {
-    socket.broadcast.emit('line', data);
-  })
+        if(users.length === 0) {
+            timeout = clearTimeout(timeout);
+        }
+    });
+    socket.on('line', data => {
+        socket.broadcast.emit('line', data);
+    });
 }
 
 function sendUsers () {
-  io.emit('users', users.map(user => {
-    return user.username;
-  }));
+    io.emit('users', users.map(user => {
+        return {
+            username: user.username,
+            isActive: currentPlayer === user
+        };
+    }));
 }
 
+function switchPlayer() {
 
+    const indexCurrentPlayer = users.indexOf(currentPlayer);
+    currentPlayer = users[(indexCurrentPlayer + 1) % users.length];
+
+    sendUsers();
+
+    const newWord = words[Math.floor(Math.random() * words.length)];
+    currentPlayer.emit('word', newWord);
+    io.emit('clear');
+
+    timeout = setTimeout(switchPlayer, 20000);
+}
